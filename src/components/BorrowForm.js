d1b950c15a0cb4dc651927b8184a6e8d40930602
Maximiliano <maxidv88@gmail.com>
2023-10-25 17:06:26 -0300

    adding lendForm borrowForm and withdrawal

--- src/components/BorrowForm.js
+++ src/components/BorrowForm.js
@@ -1,19 +1,60 @@
+"use client";
+
+import React, { useState } from "react";
+import { uintCV, PostConditionMode } from "@stacks/transactions";
+import { openContractCall } from "@stacks/connect";
+import { StacksMocknet, StacksTestnet } from "@stacks/network";
+
 export default function BorrowForm() {
+  const [amount, setAmount] = useState(0);
+
+  const handleDeposit = async () => {
+    const functionArgs = [
+      uintCV(amount), // Convert the amount to uintCV
+    ];
+
+    const contractAddress = "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM"; // Replace with your contract address
+    const contractName = "lagoon"; // Replace with your contract name
+    const functionName = "borrow"; // Function for deposit
+
+    const options = {
+      contractAddress,
+      contractName,
+      functionName,
+      functionArgs,
+      network: new StacksTestnet(),
+      //network: new StacksMocknet(),
+      postConditionMode: PostConditionMode.Allow,
+      appDetails: {
+        name: "Lagoon",
+        icon: "https://freesvg.org/img/bitcoin.png", // You can provide an icon URL for your application
+      },
+      onFinish: (data) => {
+        console.log(data);
+      },
+    };
+
+    await openContractCall(options);
+  };
+
   return (
-    <form className="flex flex-col items-center space-y-4">
+    <form
+      className="flex items-center justify-center space-x-4"
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleDeposit();
+      }}
+    >
       <input
         type="number"
         placeholder="Amount to borrow"
-        className="w-1/3 px-4 py-2 text-gray-300 bg-gray-700 rounded  focus:outline-none focus:border-orange-500"
-      />
-      <input
-        type="number"
-        placeholder="Collateral amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
         className="w-1/3 px-4 py-2 text-gray-300 bg-gray-700 rounded focus:outline-none focus:border-orange-500"
       />
       <button
         type="submit"
-        className="w-1/3 px-6 py-2 bg-orange-500 rounded hover:bg-orange-600 focus:outline-none"
+        className="px-6 py-2 bg-orange-500 rounded hover:bg-orange-600 focus:outline-none"
       >
         Borrow sBTC
       </button>
